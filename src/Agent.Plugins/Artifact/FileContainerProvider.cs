3f79213cde2b1c652b193ec306c440a6057e0441
Denis Tikhomirov <v-dentikhomi@microsoft.com>
2021-11-15 12:54:59 +0300

    Corrected GetArtifactItems - moved logic for handling of patterns to separate function GetFilteredItems. Previous function GetFilteredItems was renamed to FilterItemsByPatterns, it's used in GetFilteredItems only.

--- src/Agent.Plugins/Artifact/FileContainerProvider.cs
+++ src/Agent.Plugins/Artifact/FileContainerProvider.cs
@@ -213,7 +213,7 @@ namespace Agent.Plugins
         }
 
         // Returns all artifact items. Uses minimatch filters specified in downloadParameters.
-        private async Task<IEnumerable<FileContainerItem>> GetArtifactItems(ArtifactDownloadParameters downloadParameters, BuildArtifact buildArtifact)
+        public async Task<IEnumerable<FileContainerItem>> GetArtifactItems(ArtifactDownloadParameters downloadParameters, BuildArtifact buildArtifact)
         {
             (long, string) containerIdAndRoot = ParseContainerId(buildArtifact.Resource.Data);
             Guid projectId = downloadParameters.ProjectId;
@@ -227,14 +227,12 @@ namespace Agent.Plugins
                 containerIdAndRoot.Item2
             );
 
-            // Hashtable to keep track of matches.
-            Hashtable map = new Hashtable();
-
             Options customMinimatchOptions;
             if (!(downloadParameters.CustomMinimatchOptions is null))
             {
                 customMinimatchOptions = downloadParameters.CustomMinimatchOptions;
-            } else
+            }
+            else
             {
                 customMinimatchOptions = new Options()
                 {
@@ -244,147 +242,8 @@ namespace Agent.Plugins
                 };
             }
 
-            foreach (string minimatchPattern in minimatchPatterns)
-            {
-                tracer.Info($"Pattern: {minimatchPattern}");
+            List<FileContainerItem> resultItems = GetFilteredItems(items, minimatchPatterns, customMinimatchOptions);
 
-                // Trim and skip empty.
-                string currentPattern = minimatchPattern.Trim();
-                if (String.IsNullOrEmpty(currentPattern))
-                {
-                    tracer.Info($"Skipping empty pattern.");
-                    continue;
-                }
-
-                // Clone match options.
-                Options matchOptions = CloneMiniMatchOptions(customMinimatchOptions);
-
-                // Skip comments.
-                if (!matchOptions.NoComment && currentPattern.StartsWith('#'))
-                {
-                    tracer.Info($"Skipping comment.");
-                    continue;
-                }
-
-                // Set NoComment. Brace expansion could result in a leading '#'.
-                matchOptions.NoComment = true;
-
-                // Determine whether pattern is include or exclude.
-                int negateCount = 0;
-                if (!matchOptions.NoNegate)
-                {
-                    while (negateCount < currentPattern.Length && currentPattern[negateCount] == '!')
-                    {
-                        negateCount++;
-                    }
-
-                    currentPattern = currentPattern.Substring(negateCount);
-                    if (negateCount > 0)
-                    {
-                        tracer.Info($"Trimmed leading '!'. Pattern: '{currentPattern}'");
-                    }
-                }
-
-                bool isIncludePattern = negateCount == 0 || 
-                    (negateCount % 2 == 0 && !matchOptions.FlipNegate) || 
-                    (negateCount % 2 == 1 && matchOptions.FlipNegate);
-
-                // Set NoNegate. Brace expansion could result in a leading '!'.
-                matchOptions.NoNegate = true;
-                matchOptions.FlipNegate = false;
-
-                // Trim and skip empty.
-                currentPattern = currentPattern.Trim();
-                if (String.IsNullOrEmpty(currentPattern))
-                {
-                    tracer.Info($"Skipping empty pattern.");
-                    continue;
-                }
-
-                // Expand braces - required to accurately interpret findPath.
-                string[] expandedPatterns;
-                string preExpandedPattern = currentPattern;
-                if (matchOptions.NoBrace)
-                {
-                    expandedPatterns = new string[] { currentPattern };
-                } else
-                {
-                    // Convert slashes on Windows before calling braceExpand(). Unfortunately this means braces cannot
-                    // be escaped on Windows, this limitation is consistent with current limitations of minimatch (3.0.3).
-                    tracer.Info($"Expanding braces.");
-                    string convertedPattern = currentPattern.Replace("\\", "/");
-                    expandedPatterns = Minimatcher.BraceExpand(convertedPattern, matchOptions).ToArray();
-                }
-
-                // Set NoBrace.
-                matchOptions.NoBrace = true;
-
-                foreach (string expandedPattern in expandedPatterns)
-                {
-                    if (expandedPattern != preExpandedPattern)
-                    {
-                        tracer.Info($"Pattern: {expandedPattern}");
-                    }
-
-                    // Trim and skip empty.
-                    currentPattern = expandedPattern.Trim();
-                    if (String.IsNullOrEmpty(currentPattern))
-                    {
-                        tracer.Info($"Skipping empty pattern.");
-                        continue;
-                    }
-
-                    string[] currentPatterns = new string[] { currentPattern };
-                    IEnumerable<Func<string, bool>> minimatcherFuncs = MinimatchHelper.GetMinimatchFuncs(
-                        currentPatterns,
-                        tracer,
-                        matchOptions
-                    );
-
-                    if (isIncludePattern)
-                    {
-                        // Apply the pattern.
-                        tracer.Info($"Applying include pattern against original list.");
-                        List<FileContainerItem> matchResults = this.GetFilteredItems(items, minimatcherFuncs);
-
-                        // Union the results.
-                        int matchCount = 0;
-                        foreach (FileContainerItem matchResult in matchResults)
-                        {
-                            matchCount++;
-                            map[matchResult.Path] = Boolean.TrueString;
-                        }
-
-                        tracer.Info($"{matchCount} matches");
-                    }
-                    else
-                    {
-                        // Apply the pattern.
-                        tracer.Info($"Applying exclude pattern against original list.");
-                        List<FileContainerItem> matchResults = this.GetFilteredItems(items, minimatcherFuncs);
-
-                        // Subtract the results.
-                        int matchCount = 0;
-                        foreach (FileContainerItem matchResult in matchResults)
-                        {
-                            matchCount++;
-                            map.Remove(matchResult.Path);
-                        }
-
-                        tracer.Info($"{matchCount} matches");
-                    }
-                }
-            }
-
-            // return a filtered version of the original list (preserves order and prevents duplication)
-            List<FileContainerItem> resultItems = new List<FileContainerItem>();
-            foreach (FileContainerItem item in items)
-            {
-                if (Convert.ToBoolean(map[item.Path]))
-                {
-                    resultItems.Add(item);
-                }
-            }
             tracer.Info($"{resultItems.Count} final results");
 
             var excludedItems = items.Except(resultItems);
@@ -512,7 +371,158 @@ namespace Agent.Plugins
             return absolutePath;
         }
 
-        private List<FileContainerItem> GetFilteredItems(List<FileContainerItem> items, IEnumerable<Func<string, bool>> minimatchFuncs)
+        public List<FileContainerItem> GetFilteredItems(List<FileContainerItem> items, string[] minimatchPatterns, Options customMinimatchOptions)
+        {
+            // Hashtable to keep track of matches.
+            Hashtable map = new Hashtable();
+
+            foreach (string minimatchPattern in minimatchPatterns)
+            {
+                tracer.Info($"Pattern: {minimatchPattern}");
+
+                // Trim and skip empty.
+                string currentPattern = minimatchPattern.Trim();
+                if (String.IsNullOrEmpty(currentPattern))
+                {
+                    tracer.Info($"Skipping empty pattern.");
+                    continue;
+                }
+
+                // Clone match options.
+                Options matchOptions = CloneMiniMatchOptions(customMinimatchOptions);
+
+                // Skip comments.
+                if (!matchOptions.NoComment && currentPattern.StartsWith('#'))
+                {
+                    tracer.Info($"Skipping comment.");
+                    continue;
+                }
+
+                // Set NoComment. Brace expansion could result in a leading '#'.
+                matchOptions.NoComment = true;
+
+                // Determine whether pattern is include or exclude.
+                int negateCount = 0;
+                if (!matchOptions.NoNegate)
+                {
+                    while (negateCount < currentPattern.Length && currentPattern[negateCount] == '!')
+                    {
+                        negateCount++;
+                    }
+
+                    currentPattern = currentPattern.Substring(negateCount);
+                    if (negateCount > 0)
+                    {
+                        tracer.Info($"Trimmed leading '!'. Pattern: '{currentPattern}'");
+                    }
+                }
+
+                bool isIncludePattern = negateCount == 0 ||
+                    (negateCount % 2 == 0 && !matchOptions.FlipNegate) ||
+                    (negateCount % 2 == 1 && matchOptions.FlipNegate);
+
+                // Set NoNegate. Brace expansion could result in a leading '!'.
+                matchOptions.NoNegate = true;
+                matchOptions.FlipNegate = false;
+
+                // Trim and skip empty.
+                currentPattern = currentPattern.Trim();
+                if (String.IsNullOrEmpty(currentPattern))
+                {
+                    tracer.Info($"Skipping empty pattern.");
+                    continue;
+                }
+
+                // Expand braces - required to accurately interpret findPath.
+                string[] expandedPatterns;
+                string preExpandedPattern = currentPattern;
+                if (matchOptions.NoBrace)
+                {
+                    expandedPatterns = new string[] { currentPattern };
+                }
+                else
+                {
+                    // Convert slashes on Windows before calling braceExpand(). Unfortunately this means braces cannot
+                    // be escaped on Windows, this limitation is consistent with current limitations of minimatch (3.0.3).
+                    tracer.Info($"Expanding braces.");
+                    string convertedPattern = currentPattern.Replace("\\", "/");
+                    expandedPatterns = Minimatcher.BraceExpand(convertedPattern, matchOptions).ToArray();
+                }
+
+                // Set NoBrace.
+                matchOptions.NoBrace = true;
+
+                foreach (string expandedPattern in expandedPatterns)
+                {
+                    if (expandedPattern != preExpandedPattern)
+                    {
+                        tracer.Info($"Pattern: {expandedPattern}");
+                    }
+
+                    // Trim and skip empty.
+                    currentPattern = expandedPattern.Trim();
+                    if (String.IsNullOrEmpty(currentPattern))
+                    {
+                        tracer.Info($"Skipping empty pattern.");
+                        continue;
+                    }
+
+                    string[] currentPatterns = new string[] { currentPattern };
+                    IEnumerable<Func<string, bool>> minimatcherFuncs = MinimatchHelper.GetMinimatchFuncs(
+                        currentPatterns,
+                        tracer,
+                        matchOptions
+                    );
+
+                    if (isIncludePattern)
+                    {
+                        // Apply the pattern.
+                        tracer.Info($"Applying include pattern against original list.");
+                        List<FileContainerItem> matchResults = this.FilterItemsByPatterns(items, minimatcherFuncs);
+
+                        // Union the results.
+                        int matchCount = 0;
+                        foreach (FileContainerItem matchResult in matchResults)
+                        {
+                            matchCount++;
+                            map[matchResult.Path] = Boolean.TrueString;
+                        }
+
+                        tracer.Info($"{matchCount} matches");
+                    }
+                    else
+                    {
+                        // Apply the pattern.
+                        tracer.Info($"Applying exclude pattern against original list.");
+                        List<FileContainerItem> matchResults = this.FilterItemsByPatterns(items, minimatcherFuncs);
+
+                        // Subtract the results.
+                        int matchCount = 0;
+                        foreach (FileContainerItem matchResult in matchResults)
+                        {
+                            matchCount++;
+                            map.Remove(matchResult.Path);
+                        }
+
+                        tracer.Info($"{matchCount} matches");
+                    }
+                }
+            }
+
+            // return a filtered version of the original list (preserves order and prevents duplication)
+            List<FileContainerItem> resultItems = new List<FileContainerItem>();
+            foreach (FileContainerItem item in items)
+            {
+                if (Convert.ToBoolean(map[item.Path]))
+                {
+                    resultItems.Add(item);
+                }
+            }
+
+            return resultItems;
+        }
+
+        private List<FileContainerItem> FilterItemsByPatterns(List<FileContainerItem> items, IEnumerable<Func<string, bool>> minimatchFuncs)
         {
             List<FileContainerItem> filteredItems = new List<FileContainerItem>();
             foreach (FileContainerItem item in items)
