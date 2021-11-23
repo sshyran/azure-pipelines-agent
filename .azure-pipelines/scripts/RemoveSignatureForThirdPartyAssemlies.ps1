7b5293a1259a2caa11be3837fc54c2a8129f8c17
Nikita Ezzhev <v-niezz@microsoft.com>
2021-11-23 10:37:33 +0300

    Fix paths

--- .azure-pipelines/scripts/RemoveSignatureForThirdPartyAssemlies.ps1
+++ .azure-pipelines/scripts/RemoveSignatureForThirdPartyAssemlies.ps1
@@ -7,8 +7,10 @@ param(
 . $PSScriptRoot\Get-SigntoolPath.ps1
 . $PSScriptRoot\RemoveSignatureScript.ps1
 
+$signtoolPath = Get-Signtool | Select -Last 1
+
 if ( ($signToolPath -ne "") -and (Test-Path -Path $signtoolPath) ) {
-  Remove-ThirdPartySignatures -SigntoolPath "$signToolPath"
+  Remove-ThirdPartySignatures -SigntoolPath "$signToolPath" -LayoutRoot "$LayoutRoot"
 } else {
 	Write-Error "$signToolPath is not a valid path"
 	exit 1
