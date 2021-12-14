b42b0589e0512bd49337fbc5a8613dd1e13bf738
Anatolii Bolshakov (Akvelon INC) <v-anbols@microsoft.com>
2021-12-14 14:48:53 +0300

    Excluded non-secret service connection fields from masking

--- src/Agent.Sdk/Util/MaskingUtil.cs
+++ src/Agent.Sdk/Util/MaskingUtil.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.TeamFoundation.DistributedTask.WebApi;
+﻿using Microsoft.VisualStudio.Services.ServiceEndpoints.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +7,33 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
 {
     public static class MaskingUtil
     {
+        /// <summary>
+        /// Returns true if endpoint authorization parameter with provided key is a secret
+        /// Masks all keys except the specific fields - for which we know that they don't contain secrets
+        /// </summary>
+        /// <param name="key">Key to check</param>
+        /// <returns>Returns true if key is a secret</returns>
         public static bool IsEndpointAuthorizationParametersSecret(string key)
         {
             return key != EndpointAuthorizationParameters.IdToken
                 && key != EndpointAuthorizationParameters.Role
                 && key != EndpointAuthorizationParameters.Scope
-                && key != EndpointAuthorizationParameters.TenantId;
+                && key != EndpointAuthorizationParameters.TenantId
+                && key != EndpointAuthorizationParameters.IdToken
+                && key != EndpointAuthorizationParameters.IssuedAt
+                && key != EndpointAuthorizationParameters.ExpiresAt
+                && key != EndpointAuthorizationParameters.ExpiresIn
+                && key != EndpointAuthorizationParameters.Audience
+                && key != EndpointAuthorizationParameters.AuthenticationType
+                && key != EndpointAuthorizationParameters.AuthorizationType
+                && key != EndpointAuthorizationParameters.AccessTokenType
+                && key != EndpointAuthorizationParameters.AccessTokenFetchingMethod
+                && key != EndpointAuthorizationParameters.UseWindowsSecurity
+                && key != EndpointAuthorizationParameters.Unsecured
+                && key != EndpointAuthorizationParameters.OAuthAccessTokenIsSupplied
+                && key != EndpointAuthorizationParameters.Audience
+                && key != EndpointAuthorizationParameters.CompleteCallbackPayload
+                && key != EndpointAuthorizationParameters.AcceptUntrustedCertificates;
         }
     }
 }
