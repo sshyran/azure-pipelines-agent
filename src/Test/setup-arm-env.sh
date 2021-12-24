c6daae0390acc205fcb6c6945436ae7559cf9b05
Nikita Ezzhev <v-niezz@microsoft.com>
2021-12-24 11:28:06 +0300

    Remove setup script

--- src/Test/setup-arm-env.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update && sudo apt install -y qemu qemu-user-static qemu-user binfmt-support && docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-docker buildx create --name testcontainer && docker buildx use testcontainer
-
-docker buildx build --file "$(Build.SourcesDirectory)/src/DockerFile-${{ parameters.arch }}" --platform linux/${{ parameters.os }}-${{ parameters.arch }} --load -t test-${{ parameters.arch }} .
