0f027eb3afe0766bc176e33e32e552bae1660807
DaniilShmelev <v-dshmelev@microsoft.com>
2022-01-11 11:56:08 +0300

    Revert "don't include logs in the archive"
    This reverts commit 563d082a13e60672af9d956fca76d34fe6d4c87f.

--- src/Agent.Worker/DiagnosticLogManager.cs
+++ src/Agent.Worker/DiagnosticLogManager.cs
@@ -123,6 +123,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
 
                 string eventLogsFile = $"{HostContext.GetDirectory(WellKnownDirectory.Diag)}/EventViewer-{ jobStartTimeUtc.ToString("yyyyMMdd-HHmmss") }.log";
                 await DumpCurrentJobEventLogs(executionContext, eventLogsFile, jobStartTimeUtc);
+
+                string destination = Path.Combine(supportFilesFolder, Path.GetFileName(eventLogsFile));
+                File.Copy(eventLogsFile, destination);
             }
 
             executionContext.Debug("Zipping diagnostic files.");
